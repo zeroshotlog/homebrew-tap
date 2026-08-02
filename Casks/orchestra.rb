cee1531939939a9c080af92d755fab958198d0ff
Wizteria <gmlfg2022@gmail.com>
2026-08-03 05:11:31 +0900

    Update orchestra cask to v0.7.0

--- Casks/orchestra.rb
+++ Casks/orchestra.rb
@@ -1,6 +1,6 @@
 cask "orchestra" do
-  version "0.6.0"
-  sha256 "a07260ab23700871cca4ae6c8d27f81c876e226f326892e6b2aedcc99f9145f6"
+  version "0.7.0"
+  sha256 "7191dd946a0c4c0be2c772ae81d55cc138ed1cb7b2411aee1e9de695f0f660f1"
 
   url "https://github.com/zeroshotlog/orchestra/releases/download/v#{version}/Orchestra_#{version}_aarch64.dmg"
   name "Orchestra"
